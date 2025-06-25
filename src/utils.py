0c21de5deec432112b4d3dd4185fdfe90c5bac2b
willtwr <wrtan.edu@gmail.com>
2025-06-25 16:41:55 +0800

    rename utils function

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def sort_pd_column(dataframe: pd.DataFrame, keywords: list[str], first: bool = True):
+def sort_column_by_keywords(dataframe: pd.DataFrame, keywords: list[str], first: bool = True):
     if first:
         new_dataframe = dataframe[[c for c in dataframe if c in keywords] + [c for c in dataframe if c not in keywords]]
     else:
